a090d87b75fe96d9b3b213b955e3496a16261eba
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-03-07 15:33:54 +0200

    tags

--- src/components/MediaList.tsx
+++ src/components/MediaList.tsx
@@ -1,4 +1,5 @@
 import { fetchAllMedia } from '@/models/mediaModel';
+import { fetchTagsByMediaId } from '@/models/tagModel';
 import Image from 'next/image';
 import Link from 'next/link';
 const MediaList = async () => {
@@ -8,10 +9,20 @@ const MediaList = async () => {
     return <p>No media found</p>;
   }
 
+  const mediaListWithTags = await Promise.all(
+    mediaList.map(async (media) => {
+      const tags = await fetchTagsByMediaId(media.media_id);
+      return {
+        ...media,
+        tags,
+      };
+    }),
+  );
+
   return (
     <section className="flex flex-col p-8">
       <ul className="grid grid-cols-3 gap-4">
-        {mediaList.map((item, index) => (
+        {mediaListWithTags.map((item, index) => (
           <li
             key={index}
             className="flex flex-col items-center border border-gray-300 p-4 shadow-lg rounded-md bg-white"
@@ -25,6 +36,7 @@ const MediaList = async () => {
             <h3 className="text-lg font-bold self-start">{item.title}</h3>
             <p>Description: {item.description}</p>
             <p>Date: {new Date(item.created_at).toLocaleDateString('fi-FI')}</p>
+            <p>Tags: {item.tags?.map((tag) => tag.tag_name).join(', ')}</p>
             <Link href={`/single/${item.media_id}`}>View</Link>
           </li>
         ))}
